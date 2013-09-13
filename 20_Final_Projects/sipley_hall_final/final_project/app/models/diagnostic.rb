08233988eb60a3674a8d51a5f811e124754fd78c
Mercer Hall <mercerhall@hotmail.com>
2013-09-13 17:59:45 -0400

    Added updated survey controller and view and writer type files

--- 20_Final_Projects/sipley_hall_final/final_project/app/models/diagnostic.rb
+++ 20_Final_Projects/sipley_hall_final/final_project/app/models/diagnostic.rb
@@ -1,8 +1,45 @@
 class Diagnostic < ActiveRecord::Base
+	# validates 	:user_id, presence: true
+	belongs_to 	:users
+
+# points method(answer)
+# 	if answer = '1' then @point = 1.to_i
+# 	...
+# end
+
+# question1 method
+# 	points(answer1)
+# 	@point = @point1
+# end
+
+# question2 method
+# 	points(answer1)
+# 	@point = @point1
+# end
+
+# question3 method
+# 	points(answer1)
+# 	@point = @point1
+# end
+
+# question4 method
+# 	points(answer1)
+# 	@point = @point1
+# end
+
+# tally method
+# @tally = @point1 + @point2 + @point3 + @point4
+# end
+
+# algorithm method
+# if .... writer_type = @Ricky
+# ...
+# end
+
+end
 	# validates , presence: true
 	# has_many :users
 	# attr_accessible :answer1, :answer2, :answer3, :answer4
-end
 
 
 #This model has to define a user_id and assign a writer_type to the user in the database
