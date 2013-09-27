aca6293a0608bb572024d93b787ddde648333538
Mercer Hall <mercerhall@hotmail.com>
2013-09-27 14:41:46 -0400

    Many updates with additions of front end code

--- 20_Final_Projects/sipley_hall_final/final_project/app/models/user.rb
+++ 20_Final_Projects/sipley_hall_final/final_project/app/models/user.rb
@@ -5,10 +5,38 @@ class User < ActiveRecord::Base
 
   #the program won't run unless a string is entered for each email, last_name, first_name. We need a more precise validation
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable
-	validates :email, :last_name, :first_name, presence: true
+         :recoverable, :rememberable, :trackable, :validatable
+	validates :email, presence: true
+  #:last_name, :first_name,
 	has_one :diagnostics
 	has_one :surveys
 	has_one :writer_types
+  
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 	# Do we need to include the above associations?
 end
+
+
