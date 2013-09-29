109cfdae5539217f21cb55d7a13647a294f71b7e
Mercer Hall <mercerhall@hotmail.com>
2013-09-29 15:31:06 -0400

    Revised front end and diagnostic, survey, continue page

--- 20_Final_Projects/sipley_hall_final/final_project/app/models/user.rb
+++ 20_Final_Projects/sipley_hall_final/final_project/app/models/user.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  first_name             :string(255)
+#  last_name              :string(255)
+#  email                  :string(255)
+#  created_at             :datetime
+#  updated_at             :datetime
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -8,7 +29,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 	validates :email, presence: true
   #:last_name, :first_name,
-	has_one :diagnostics
+	has_many :diagnostics
 	has_one :surveys
 	has_one :writer_types
   
